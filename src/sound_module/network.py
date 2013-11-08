f9ae6aead1213aab8c67c2889355ca79183f0c82
tkb <devnull@localhost>
2013-11-08 14:39:02 +0100

    Second try, disallowed loops

--- src/sound_module/network.py
+++ src/sound_module/network.py
@@ -1,54 +1,47 @@
-from sound_module.streams import Reverse, Delay, ArrayStream, Stream
+from sound_module.streams import Reverse, Delay, Operation, Input
     
-class Module(object):
-    def __init__(self, module_id, stream):
-        self.__id = module_id
-        self.__stream = stream
-        
-    def get_id(self):
-        return self.__id
-
-    def add_input(self, new_input):
-        self.__stream.with_input(new_input.__stream)
-        
-    def output(self, time):
-        return self.__stream.output(time)
-    
-    def _get_stream(self):
-        return self.__stream
-    
-class Network(Stream):
+class Network(Operation):
     def __init__(self, array_input):
-        self.__input = Module("input", array_input)
-        self.__modules = {}
+        self.__modules = {"#INPUT": Input(array_input)}
+        self.__module_order = ("#INPUT", )
+        self.__connections = {}
         self.__output = array_input
         self.__first = True
         
-    def add_module(self, module):
+    def add_module(self, module_id, module):
+        self.__modules[module_id] = module
+        self.__module_order = self.__module_order + (module_id, )
+        self.__output = module_id
         if self.__first:
-            module.add_input(self.__input)
             self.__first = False
-        self.__modules[module.get_id()] = module
-        self.__output = module
+            self.add_connection("#INPUT", module_id)
         
     def add_connection(self, module_out_id, module_in_id):
-        input_module = self.__modules[module_in_id]
-        output_module = self.__modules[module_out_id]
-        input_module.add_input(output_module)
+        if self.__module_order.index(module_out_id) > self.__module_order.index(module_in_id):
+            raise ValueError("""Modules can be only connected in the order of their definition: 
+                Tried to connect %s to %s""", module_out_id, module_in_id)
+        if module_in_id in self.__connections:
+            self.__connections[module_in_id] = self.__connections[module_in_id] + (module_out_id, )
+        else:
+            self.__connections[module_in_id] = (module_out_id, )
         
-    def output(self, time):
-        return self.__output.output(time)
+    def process(self):
+        out = None
+        count = 0
+        while out != "" and count < 16:
+            out = self.__fetch(self.__output)
+            yield out
+        
+    def __fetch(self, module):
+        inputs = self.__connections[module] if module in self.__connections else tuple() 
 
+        return self.__modules[module].process(map(self.__fetch, inputs))
+        
 if __name__ == "__main__":
-    inputs = ArrayStream(("Hello", "World", ""))
+    inputs = ("Hello", "World")
     net = Network(inputs)
-    net.add_module(Module("A", Reverse()))
-    net.add_module(Module("B", Delay()))
+    net.add_module("A", Reverse())
+    net.add_module("B", Delay())
     net.add_connection("A", "B")
-    net.add_connection("B", "A")
     
-    print(net.output(0))
-    print(net.output(1))
-    print(net.output(2))
-    print(net.output(3))
-    print(net.output(8))
+    print(list(net.process()))
--- src/sound_module/streams.py
+++ src/sound_module/streams.py
@@ -1,87 +1,45 @@
-from abc import ABCMeta, abstractmethod
-
-class Stream(object):
-    __metaclass__ = ABCMeta
-    
-    @abstractmethod
-    def output(self, time):
+class Operation(object):
+    def process(self, inputs):
         raise NotImplementedError
-    
-class ArrayStream(Stream):
+
+class Input(Operation):
     def __init__(self, values):
         self.__values = values
+        self.__count = 0
+    
+    def process(self, inputs):
+        if self.__count < len(self.__values):
+            value = self.__values[self.__count]
+            self.__count += 1
+            
+            return value
         
-    def output(self, time):
-        return self.__values[time] if time < len(self.__values) else ""
+        return ""
     
-class NetworkStream(Stream):
-    def __init__(self, network):
-        self.__network = network
-    
-    def output(self, time):
-        return self._network.get_output().output(time)
-    
-class InputStream(Stream):
-    def __init__(self, inputs):
-        self.__inputs = inputs
-        
-    def with_input(self, new_input):
-        #return Stream(self.__inputs + (new_input, ))
-        self.__inputs = self.__inputs + (new_input, )
-    
-    def _get_inputs(self):
-        return self.__inputs
-    
-class Adder(InputStream):
-    def __init__(self, inputs = tuple()):
-        super(InputStream, self).__init__(inputs)
+class Adder(Operation):
+    def process(self, inputs):
+        return "".join(inputs)
 
-    def output(self, time):
-        out = ""
-        for x in self._get_inputs():
-            out = out + x.output(time)
-        
-        return out
-        #return sum([i.output(time) for i in self._get_inputs()])
-
-class Delay(Adder):
-    def __init__(self, inputs = tuple()):
-        super(Adder, self).__init__(inputs)
-        self.__time = -1
+class Delay(Operation):
+    def __init__(self):
         self.__previous = "Hello"
 
-    def output(self, time):
-        if time == self.__time:
-            return self.__previous
+    def process(self, inputs):
+        previous = self.__previous
+        self.__previous = Adder().process(inputs)
         
-        if time == self.__time + 1:
-            self.__time = time
-            previous = self.__previous
-            self.__previous = Adder.output(self, time)
-            
-            return previous
-                
-        return None
+        return previous
     
-class Echo(Adder):
-    def __init__(self, inputs = tuple()):
-        super(Adder, self).__init__(inputs)
-
-    def output(self, time):
-        inputs = Adder.output(self, time)
+class Echo(Operation):
+    def process(self, inputs):
+        added_inputs = Adder().process(inputs)
         
-        return inputs + inputs
+        return added_inputs + added_inputs
             
-class Reverse(Adder):
-    def __init__(self, inputs = tuple()):
-        super(Adder, self).__init__(inputs)
-
-    def output(self, time):
-        return Adder.output(self, time)[::-1]
+class Reverse(Operation):
+    def process(self, inputs):
+        return Adder().process(inputs)[::-1]
         
-class Noop(Adder):
-    def __init__(self, inputs = tuple()):
-        super(Adder, self).__init__(inputs)
-
-    def output(self, time):
-        return Adder.output(self, time)
+class Noop(Operation):
+    def process(self, inputs):
+        return Adder().process(inputs)
